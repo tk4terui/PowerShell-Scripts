482ca1d09c6eef1557b0291c41e852687f7fada1
tk4terui <tk4terui@gmail.com>
2023-05-01 12:40:04 +0900

    update

---
+++ FileOperation/getFileHashRecursiveInTargetDir.ps1
@@ -0,0 +1,6 @@
+﻿$targetDir = ""C:\Users\terui.takeshi\Downloads\ISO""
+$files = Get-ChildItem $targetDir -File
+
+foreach ($file in $files){
+    Write-Host "$($file): $((Get-FileHash $file.FullName -Algorithm md5).Hash.toLower())"
+}
